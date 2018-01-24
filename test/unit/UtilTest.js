64c90c8b13d5e1a4e3c257319f78b0cf1c998896
samsepiol <samsepiol@samsepiol.samsepiol>
2018-01-24 14:16:25 +0100

    Before Util

--- test/unit/UtilTest.js
+++ test/unit/UtilTest.js
@@ -85,7 +85,70 @@ describe("Factorial", function(){
  */
 Util.arrangement = function(n, r) {
 
-    
+};
 
-    return Util.factorial(n) / Util.factorial(n - r)
+/**
+ * Calcule la disposition non ordonnée de r éléments dans un ensemble de n elements.
+ * Formule: Util.combination(n, r) = n! / (n - r)!r!
+ * @param {number} n
+ * @param {number} r
+ * @returns {number}
+ */
+Util.combination = function(n, r) {
+
+};
+
+/**
+ * Détermine si n est un nombre premier.
+ * Util.isPrime(5) => false
+ * Util.isPrime(6) => true
+ *
+ * @param {number} n
+ * @returns {boolean}
+ */
+Util.isPrime = function(n) {
+
+};
+
+
+/**
+ * Additionne l'ensemble des nombres de 2 à n
+ *
+ * Util.sumPrime(6) = 2 + 3 + 5 = 10
+ * Util.sumPrime(8) = 2 + 3 + 5 + 7 = 17
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+Util.sumPrime = function(n) {
+
+};
+
+/**
+ * Cette méthode doit retourner un tableau de 1 à n tel que:
+ * - Pour les nombres multiples de 3, les remplacer par "Fizz"
+ * - Pour les nombres multiples de 5, les remplacer par "Buzz"
+ * - Pour les nombres multiples de 3 et 5, les remplacer par "FizzBuzz"
+ *
+ * Exp :
+ * Util.fizzBuzz(15) => [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]
+ *
+ * @param {number} n
+ * @returns {array}
+ */
+Util.fizzBuzz = function(n) {
+
+};
+
+/**
+ * Chiffre une phrase selon la règle suivante : Les A deviennent des B, les B des C, etc.
+ *
+ * Exp :
+ * Util.cipher("Test Unitaire") => "Uftu Tojubjsf"
+ *
+ * @param phrase
+ * @returns {string}
+ */
+Util.cipher = function (phrase) {
+
 };
